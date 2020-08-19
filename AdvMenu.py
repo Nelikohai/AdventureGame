3f181b90de49d402be9d6e774a848b83eb2c1f8a
BlastedGlass07 <69607761+BlastedGlass07@users.noreply.github.com>
2020-08-19 17:00:30 -0400

    Add files via upload

--- AdvMenu.py
+++ AdvMenu.py
@@ -8,28 +8,28 @@ pygame.init()
 pygame.display.set_caption("Adventure Game")
 screen = pygame.display.set_mode((500, 500), 0, False)
 rootDirectory = os.path.dirname(__file__)
-imageDirectory = os.path.join(rootDirectory + "\\img")
+imageDirectory = rootDirectory + '\\img'
 textDirectory = rootDirectory + '\\text'
 
-TitleFont = pygame.font.SysFont("", 50)
-NormalFont = pygame.font.SysFont("", 20)
 
-#Clicks
-click = False
-rightClick = False
+
+TitleFont = pygame.font.SysFont("", 50)
+NormalFont = pygame.font.SysFont("", 30)
 
 #Title
 Title = TitleFont.render("Adventure Game", 1, (255, 255, 255))
 Titlerect = Title.get_rect()
 Titlerect.topleft = (20, 20)
-screen.blit(Title,Titlerect)
+
+#Clicks
+click = False
+rightClick = False
 
 #Button
 btnStart = pygame.draw.rect(screen, (100, 250, 50), (30, 175,150, 40))
 txtStart = NormalFont.render("Start", 1, (0, 0, 0))
 txtStartrect = txtStart.get_rect()
 txtStartrect.center = (100, 196)
-screen.blit(txtStart, txtStartrect)
 #Buttons for Answers
 btnAnswer1 = None
 btnAnswer2 = None
@@ -47,13 +47,10 @@ def grabFile(fileName):
 
     #[Type of sentence (sentence/question)] [The actual words said}
     dialogue = open(textDirectory + '\\' + fileName + '.txt')
-
     #local variables that can be used in either if statement
     myCount = 0
     tempArray1 = []
-    textArray = []
-    #Start making changes here
-
+    
     if fileName == "text":
         #local variables that can only be used in this if statement
         tempArray2 = []
@@ -63,8 +60,8 @@ def grabFile(fileName):
                 tempArray2.insert(myCount, dialogue.readline())
                 myCount = myCount + 1
 
-        textArray = [tempArray1, tempArray2]    
 
+        textArray = [tempArray1, tempArray2]
     elif fileName == "questions":
         #local variable
         #TODO: changeing Delimiters to a comma
@@ -74,10 +71,10 @@ def grabFile(fileName):
                 tempString = dialogue.readline()
             else:
                 myCount = myCount + 1
-        print(tempString)
-                
+        print(tempString)        
         #We need to turn the string into a delimited array
         textArray = tempString.split(",")
+
                 
     dialogue.close()
     return textArray
@@ -106,15 +103,16 @@ def createText(text):
     screen.blit(txtBox, txtBoxrect)
 
 #create buttons to click for answering questions
-def createAnswers(text):
+#def createAnswers(text):
     #parameter: Array that has the answers in them
     #TODO: Create as amny buttons as needed
     #Create text on top of the buttons
     #Be able to click the buttons
 
+
+
 def gameScreen():
     secondWhile = True
-    firstMessage = True
     clickCount = 0
     click = False
     questionCount = 0
@@ -124,14 +122,16 @@ def gameScreen():
         #makes the background black
         screen.fill((0, 0, 0))
         
-        myImage = pygame.transform.smoothscale(pygame.image.load(imageDirectory + "\\cliff.jpg"), (500, 350))
+        myImage = pygame.transform.smoothscale(
+            pygame.image.load(imageDirectory + "\\cliff.jpg")
+            , (500, 350)
+        )
         screen.blit(myImage,(0, 0))                  #           x   y       width  height
         pygame.draw.rect(screen, (255, 255, 255), (0, 350, 500, 150), 0)
         pygame.draw.rect(screen, (0, 0, 0), (10, 360, 480, 130), 0)
         
 
         mouseX, mouseY = fpsAndMouse()
-
 
         #Where to do run game
 
@@ -148,24 +148,25 @@ def gameScreen():
                 
                 questionCount = questionCount + 1
                 clickcounter = False
+        #if the category is sentence
         elif myText[0][clickCount]=="Sentence\n":
                 createText(myText[1][clickCount])
 
         if clickcounter == True:
 
-            #goes to the next index when you click
+        #goes to the next index when you click
             if click == True:
                 clickCount = clickCount + 1
 
-        #goes to the next index when you click
-        if click == True:
 
-            clickCount = clickCount + 1
-
-        #where to stop the game
+        #where to stop the code for gameplay
         click = False
 
+
         rightClick = False
+
+
+
 
 
         for event in pygame.event.get():
@@ -179,18 +180,26 @@ def gameScreen():
             if event.type == MOUSEBUTTONDOWN:
                 if event.button == 1:
                     click = True
+                else:
+                    rightClick = True
 
         pygame.display.update()
         mainClock.tick(60)
+
+
+
+
+
 
 
 def titleScreen():
     firstWhile = True
     while firstWhile == True:
 
-        #makes the background black
+        #TODO: make some cool backgrounds
         img0 = pygame.image.load(imageDirectory + "\\vinedbrick.png")
         screen.blit(img0, (0, 0))
+
 
         #draws the button
         btnStart = pygame.draw.rect(screen, (100, 250, 50), (30, 175,150, 40))
@@ -206,6 +215,9 @@ def titleScreen():
                 #do something
                 firstWhile = False
                 gameScreen()
+
+
+
 
 
         click = False
@@ -225,6 +237,7 @@ def titleScreen():
         #updates the display
         pygame.display.flip()
         mainClock.tick(60)
+
 
 myText = grabFile("text")
 titleScreen()
